5375f98500c239845b4d521d024623aff65daea3
Shiva Munigala <munigalashiva28@gmail.com>
2020-05-20 16:05:28 +0530

    finally done

--- ModuleOne/app/src/main/java/com/example/myapplication/Decomp2Nf.java
+++ ModuleOne/app/src/main/java/com/example/myapplication/Decomp2Nf.java
@@ -4,6 +4,8 @@ import android.os.Build;
 
 import androidx.annotation.RequiresApi;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -15,35 +17,179 @@ import java.util.stream.Collectors;
 public class Decomp2Nf {
 
     @RequiresApi(api = Build.VERSION_CODES.N)
-    public static Set<String> decomp2nf(HashMap<String, String> fds, String rls, Set<String> ckset)
-    {
-        Set<Set<Character>> ckSetSet = ckset.stream().map(s -> s.chars().mapToObj(c -> (char) c).collect(Collectors.toSet())).collect(Collectors.toSet());
-        Set<Character> ckSetCons = ckSetSet.stream().reduce(new HashSet<>(), (s, e) -> {s.addAll(e); return s;}, (s1, s2) -> {s1.addAll(s2); return s1;});
+    public Set<String> decomp2nf(HashMap<String, String> fds, String rls, Set<String> ckset){
+        Character[] rlsarr = ArrayUtils.toObject(rls.toCharArray());
+        Set<Character> rlsset = new HashSet<Character>(Arrays.asList(rlsarr));
+
+        Set<Character> pset = new HashSet<Character>();
+        for(String x : ckset)
+        {
+            Character[] templ = ArrayUtils.toObject(x.toCharArray());
+            for(Character y : templ)
+                pset.add(y);
 
 
-        Map<Set<Character>, Set<Character>> mspSets = new HashMap<>();
-        fds.forEach((k, v) -> {
-            mspSets.put(k.chars().mapToObj(c -> (char) c).collect(Collectors.toSet()), v.chars().mapToObj(c -> (char) c).collect(Collectors.toSet()));
-        });
+        }
 
-        Set<Character> rlsSet = rls.chars().mapToObj(c -> (char) c).collect(Collectors.toSet());
-        Set<String> dec = new HashSet<>();
+        String x;
 
-        mspSets.forEach((k,v) -> {
-            if(!ckSetCons.containsAll(v)) {
-                if(ckSetSet.stream().anyMatch(e -> e.containsAll(k) && !e.equals(k))) {
-                    v.removeAll(ckSetCons);
-                    rlsSet.removeAll(v);
-                    v.addAll(k);
-                    dec.add(v.stream().map(String::valueOf).collect(Collectors.joining()));
+        String temp = "";
+        String temps = "";
+
+        Set<String> relations = new HashSet<String>();
+        int n = fds.size();
+        int[] flag = new int[n];
+        for(int i=0; i<n; i++)
+            flag[i] = 0;
+        int count=0;
+        for(Map.Entry<String, String> entry : fds.entrySet())
+        {
+            x = entry.getKey();
+            for(String ck : ckset)
+            {
+                if(checksubstring(ck,x))
+                {
+                    Set<Character> tempset = new HashSet<>(Arrays.asList(ArrayUtils.toObject(entry.getValue().toCharArray())));
+                    for(Character c : tempset)
+                    {
+                        if(!pset.contains(c))
+                            rlsset.remove(c);
+                    }
+                    relations.add(x.concat(entry.getValue()));
+                    flag[count] = 1;
+                    break;
                 }
             }
-        });
+            count++;
+        }
 
-        dec.add(rlsSet.stream().map(String::valueOf).collect(Collectors.joining()));
+        count = 0;
+        for(Map.Entry<String, String> entry : fds.entrySet())
+        {
+            x = entry.getKey();
+            int flagck = 0;
+            for(String ck : ckset)
+            {
+                if(checkstring(x,ck))
+                    flagck = 1;
+            }
 
-        return dec;
+            if(flagck==0 && flag[count]==0)
+            {
+                String tempval = "";
+
+                for (String s : relations) {
+                    if(checkstring(s,x))
+                    {
+                        temps = new String(s);
+                        temp = new String(s);
+                        for(char c : entry.getValue().toCharArray())
+                        {
+                            if(temp.indexOf(c)==-1)
+                            {
+                                temp = temp + c;
+                                tempval = tempval + c;
+
+                            }
+                        }
+                        break;
+                    }
+                }
+
+                if(!temp.isEmpty())
+                {
+                    Set<Character> tempset = new HashSet<Character>(Arrays.asList(ArrayUtils.toObject(tempval.toCharArray())));
+                    for(Character c : tempset)
+                    {
+                        if(!pset.contains(c))
+                            rlsset.remove(c);
+                    }
+                    System.out.println(rlsset.toString());
+                    relations.remove(temps);
+                    relations.add(temp);
+                }
+            }
+            count++;
+        }
+
+
+        Character[] rfin = new Character[rlsset.size()];
+        rlsset.toArray(rfin);
+        String rfinnew = new String(ArrayUtils.toPrimitive(rfin));
+        relations.add(rfinnew);
+        System.out.println(relations.toString());
+        return relations;
     }
+
+    public boolean sameChars(String firstStr, String secondStr)
+    {
+        char[] first = firstStr.toCharArray();
+        char[] second = secondStr.toCharArray();
+        Arrays.sort(first);
+        Arrays.sort(second);
+        return Arrays.equals(first, second);
+    }
+
+    public boolean checksubstring(String s1, String s2)
+    {
+        char[] first = s1.toCharArray();
+        char[] second = s2.toCharArray();
+        int flag = 0;
+        for(char c : second)
+        {
+            if(s1.indexOf(c)==-1)
+            {
+                flag = 1;
+            }
+        }
+        return (flag==0 && s1.length()>s2.length());
+    }
+
+    public boolean checkstring(String s1, String s2)
+    {
+        char[] first = s1.toCharArray();
+        char[] second = s2.toCharArray();
+        int flag = 0;
+        for(char c : second)
+        {
+            if(s1.indexOf(c)==-1)
+            {
+                flag = 1;
+            }
+        }
+        return (flag==0 );
+    }
+
+//        Set<Set<Character>> ckSetSet = ckset.stream().map(s -> s.chars().mapToObj(c -> (char) c).collect(Collectors.toSet())).collect(Collectors.toSet());
+//        Set<Character> ckSetCons = ckSetSet.stream().reduce(new HashSet<>(), (s, e) -> {s.addAll(e); return s;}, (s1, s2) -> {s1.addAll(s2); return s1;});
+//
+//
+//        Map<Set<Character>, Set<Character>> mspSets = new HashMap<>();
+//        fds.forEach((k, v) -> {
+//            mspSets.put(k.chars().mapToObj(c -> (char) c).collect(Collectors.toSet()), v.chars().mapToObj(c -> (char) c).collect(Collectors.toSet()));
+//        });
+//
+//        Set<Character> rlsSet = rls.chars().mapToObj(c -> (char) c).collect(Collectors.toSet());
+//        Set<String> dec = new HashSet<>();
+//
+//        mspSets.forEach((k,v) -> {
+//            if(!ckSetCons.containsAll(v)) {
+//                if(ckSetSet.stream().anyMatch(e -> e.containsAll(k) && !e.equals(k))) {
+//                    v.removeAll(ckSetCons);
+////                    if(mspSets.containsKey(v)){
+////                        v.addAll(mspSets.get(v));
+////                    }
+//                    rlsSet.removeAll(v);
+//                    v.addAll(k);
+//                    dec.add(v.stream().map(String::valueOf).collect(Collectors.joining()));
+//                }
+//            }
+//        });
+//
+//        dec.add(rlsSet.stream().map(String::valueOf).collect(Collectors.joining()));
+//
+//        return dec;
+//    }
         //HashMap<Set<Character>,Set<Character>> hashMap =
 //        String x;
 //        String temp = new String();
--- ModuleOne/app/src/main/java/com/example/myapplication/DecompKeys.java
+++ ModuleOne/app/src/main/java/com/example/myapplication/DecompKeys.java
@@ -11,26 +11,51 @@ public class DecompKeys {
     {
         HashMap<String, Set<String>> finalmap = new HashMap<String, Set<String>>();
         CandidateKey ck = new CandidateKey();
-
+        Closure closure = new Closure();
         for(String rl : relations)
         {
             HashMap<String, String> tempfd = new HashMap<String,String>();
-
-            for(Map.Entry<String, String> entry : fds.entrySet())
+            int index,lt,flag;
+            char[] rlarr = rl.toCharArray();
+            int[] sel = new int[rl.length()];
+            String temp;
+            String s;
+            for(int i=0;i<Math.pow(2,rl.length())-1;i++)
             {
-                Set<Character> s = new HashSet<Character>();
-                if(containschar(rl,entry.getKey()))
-                {
-                    for(char ch : entry.getValue().toCharArray())
-                    {
-                        if(rl.indexOf(ch)!=-1)
-                            s.add(ch);
-                    }
+                for(int l=0; l<rl.length(); l++)
+                    sel[l] = 0;
 
-                    Character[] sarr = new Character[s.size()];
-                    s.toArray(sarr);
-                    String val = new String(ArrayUtils.toPrimitive(sarr));
-                    tempfd.put(entry.getKey(), val);
+                index = 0;
+                temp = new String();
+
+                lt = i;
+                while(lt>0)
+                {
+                    sel[index++] = lt%2;
+                    lt = lt/2;
+                }
+
+                for(int l=0; l<rl.length(); l++)
+                {
+                    if(sel[l]==1)
+                    {
+                        temp = temp + rlarr[l];
+                    }
+                }
+
+                s = closure.findclosure(fds, temp);
+                String sval = new String();
+                for(char ch : s.toCharArray())
+                {
+                    if(rl.indexOf(ch)!=-1 && temp.indexOf(ch)==-1)
+                    {
+                        sval = sval+ch;
+                    }
+                }
+
+                if(!sval.isEmpty())
+                {
+                    tempfd.put(temp, sval);
                 }
             }
 
@@ -42,15 +67,49 @@ public class DecompKeys {
 
         return finalmap;
     }
-
-    public boolean containschar(String rl, String s)
-    {
-        for(char c : s.toCharArray())
-        {
-            if(rl.indexOf(c)==-1)
-                return false;
-        }
-
-        return true;
-    }
 }
+//        HashMap<String, Set<String>> finalmap = new HashMap<String, Set<String>>();
+//        CandidateKey ck = new CandidateKey();
+//
+//        for(String rl : relations)
+//        {
+//            HashMap<String, String> tempfd = new HashMap<String,String>();
+//
+//            for(Map.Entry<String, String> entry : fds.entrySet())
+//            {
+//                Set<Character> s = new HashSet<Character>();
+//                if(containschar(rl,entry.getKey()))
+//                {
+//                    for(char ch : entry.getValue().toCharArray())
+//                    {
+//                        if(rl.indexOf(ch)!=-1)
+//                            s.add(ch);
+//                    }
+//
+//                    Character[] sarr = new Character[s.size()];
+//                    s.toArray(sarr);
+//                    String val = new String(ArrayUtils.toPrimitive(sarr));
+//                    tempfd.put(entry.getKey(), val);
+//                }
+//            }
+//
+//            Set<String> ckset;
+//            ckset = ck.findCandidateKey(tempfd, rl);
+//
+//            finalmap.put(rl, ckset);
+//        }
+//
+//        return finalmap;
+//    }
+//
+//    public boolean containschar(String rl, String s)
+//    {
+//        for(char c : s.toCharArray())
+//        {
+//            if(rl.indexOf(c)==-1)
+//                return false;
+//        }
+//
+//        return true;
+//    }
+
